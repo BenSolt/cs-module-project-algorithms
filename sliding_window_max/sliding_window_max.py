fce8c2cb26e244d780b3e10194e7f381cdb6677c
benjamin-solt <bensolt@gmail.com>
2020-05-20 15:32:37 -0400

    workin on sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,7 +5,18 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    # k = to how big array of group nums selected is [ 0,0,0 ] this case = 3.
+
+    maxx = 0
+    numbs = len(nums)
+
+    for i in range(numbs - k + 1):
+        maxx = nums[i]
+        for j in range(1, k):
+            if nums[i + j] > maxx:
+                maxx = nums[i + j]
+        print(maxx) 
+
 
 
 if __name__ == '__main__':
