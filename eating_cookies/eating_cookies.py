982af720564a8954c828b0aec299a1c41bc57cd1
benjamin-solt <bensolt@gmail.com>
2020-05-20 14:23:25 -0400

    cookies eat 3 ways

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,15 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
 
-    #pass
+ # number of ways cookie monster can eat cookes.  
+ # First = 1+1+1, Second = 2+1, Third = 1+2
     if i == 0:
+        return 1
+
+    #total number of ways cookie monster can eat cookies = 3. 
+    #    
+    
+    print(eating_cookies(10))
 
 
 if __name__ == "__main__":
